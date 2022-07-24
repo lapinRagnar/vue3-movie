2512e23f46d16a0c259d9623aceb0ac40fa1fc39
lapinRagnar <lapinragnar@gmail.com>
2022-07-24 23:59:02 +0200

    suite HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,16 @@
 
     <div class="movies-list">
       <div class="movie" v-for="(movie,i) in movies" :key="i">
-        {{ movie.Title }}
+        <router-link class="movie-link" to="'/movie/' + movie.imdbID">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="movie poster">
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="y">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
       </div>
     </div>
   </div>
@@ -146,5 +155,64 @@ export default {
       }
     }
   }
+
+  .movies-list{
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 8px;
+
+    .movie{
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link{
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image{
+          position: relative;
+          display: block;
+
+          img{
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type{
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: #fff;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail{
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0 0 8px 8px;
+
+          .y{
+            color: #aaa;
+            font-size: 14px;
+          }
+
+          h3{
+            color: #fff;
+            font-weight: 600;
+            font-size: 18px;
+          }
+        }
+      }
+    }
+  }
+
 }
 </style>
